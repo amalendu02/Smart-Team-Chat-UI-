7fe83b94e25bf98d084792f628fab3ea3b7bab9c
amalendu02 <amalendumaiti14@gmail.com>
2025-09-05 00:11:23 +0530

    Done with the side bar

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+import Avatar from '@mui/material/Avatar';
+import React from 'react'
+
+const Cat = () => {
+  return (
+    <Avatar src = "https://pixabay.com/photos/cat-domestic-animal-animal-mammal-8257177/"/>
+  )
+}
+
+export default Cat
---
+++ src/pages/dashboard/GeneralApp.js
@@ -0,0 +1,16 @@
+import React, {Suspense, lazy} from "react";
+
+// Dynamic import
+const Cat = lazy(() => import("../../components/Cat"));
+const GeneralApp = () => {
+
+  return (
+    <>
+      <Suspense fallback="Loading...">
+        <Cat />
+      </Suspense>
+    </>
+  );
+};
+
+export default GeneralApp;
